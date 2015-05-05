87357500f5cc039dbf796513531e54fd5c0ec7ab
FiodarM <fyodormorozko95@gmail.com>
2015-05-05 18:09:04 +0300

    Initial commit

--- lab7_8.py
+++ lab7_8.py
@@ -1,21 +1,87 @@
 __author__ = 'fiodar'
 
 from wave_equation import *
-from numpy import exp
+from numpy import exp, sin, cos, pi
 import matplotlib.pyplot as plt
-
+import matplotlib.animation as animation
+from matplotlib.widgets import Slider, Button
 
 a = 1.
 f = lambda x, t: exp(-x**2*t) * (2*a*t - 4 * a * t**2 * x**2 - x**2)
+f_simple = lambda x, t: 0 * x + 0 * t
+x_conditions = [lambda t: 1 + 0 * t, lambda t: 0 + exp(-t)]
+x_simple_conditions = [lambda t: -1 + 0 * sin(t), lambda t: 0 * t]
+t_conditions = [lambda x: 1 + 0 * x, lambda x: -x ** 2]
+t_simple_conditions = [lambda x: -1 + x, lambda x: 0 * x]
+
 x_bounds = [0, 1]
-t_bounds = [0, 1]
-x_conditions = [lambda t: 1 + 0*t, lambda t: exp(-t)]
-t_conditions = [lambda x: 1 + 0*x, lambda x: -x**2]
+t_bounds = [0, 10]
+x = np.linspace(x_bounds[0], x_bounds[1], 200)
+t = np.linspace(t_bounds[0], t_bounds[1], 100)
 
-x = np.linspace(x_bounds[0], x_bounds[1], 100)
-t = np.linspace(t_bounds[0], t_bounds[1], 50)
+u = wave_equation_solve_1d(a, f_simple, x, t, x_simple_conditions, t_simple_conditions)
 
-u = wave_equation_solve_1d(a, f, x_bounds, t_bounds, x_conditions, t_conditions, 10, 10)
 
-plt.plot(x, u[-1])
+# plotting
+fig = plt.figure()
+plt.subplots_adjust(bottom=0.25)
+ax = fig.gca(xlim=(0, 1), ylim=(-3, 4))
+ax.set_xlabel("x")
+ax.set_ylabel("u")
+l, = ax.plot(x, u[0], 'b-', lw=2)
+
+
+def animate_wave(time, data, line):
+    x, u = data
+    stime.set_val(t_bounds[1] * float(time) / len(t))
+    line.set_data(x, u[time])
+    return line,
+
+
+def init():
+    l.set_data([], [])
+    return l,
+
+
+axtime = plt.axes([0.2, 0.1, 0.65, 0.03])
+stime = Slider(axtime, 'time', t_bounds[0], t_bounds[1] * (1 - 1. / len(t)), valinit=t_bounds[0])
+
+
+def update(val):
+    time = stime.val
+    l.set_data(x, u[int(time / t_bounds[1] * len(t))])
+
+
+stime.on_changed(update)
+
+
+
+# playax = plt.axes([0.69, 0.025, 0.1, 0.04])
+# button_play = Button(playax, 'Play', color='w', hovercolor='0.975')
+#
+# anim = animation.FuncAnimation(fig, animate_wave, frames=len(t),
+# init_func=init, fargs=([x, u], l), blit=True, interval=t_bounds[1]*100/fps)
+#
+# def play(event):
+# anim._start()
+#
+# def stop(event):
+#     l.set_data(x, u[int(stime.val/t_bounds[1]*len(t))])
+#
+# button_play.on_clicked(play)
+
+resetax = plt.axes([0.8, 0.025, 0.1, 0.04])
+button_reset = Button(resetax, 'Reset', color='w', hovercolor='0.975')
+
+
+def reset(event):
+    stime.reset()
+
+
+button_reset.on_clicked(reset)
+
+# anim = animation.FuncAnimation(fig, animate_wave, frames=len(t),
+#                                init_func=init, fargs=([x, u], l), blit=True, interval=t_bounds[1]*100/fps)
+# anim.save('solution.mp4', writer='ffmpeg', fps=fps)
+
 plt.show()
--- wave_equation.py
+++ wave_equation.py
@@ -24,46 +24,15 @@ def tridiag_solve(diags, f):
     return x
 
 
-def ode_linear_2nd_order(coefs, bounds, conditions, n=50, f=lambda x: 0 * x):
-    p, q = coefs
-    x = np.linspace(bounds[0], bounds[1], n)
-    h = x[1] - x[0]
-    alpha, beta = conditions
-    a = [alpha[0] - 1.5 * alpha[1] / h, 0.5 * beta[1] / h]
-    b = [2 * alpha[1] / h, -2 * beta[1] / h]
-    c = [- 0.5 * alpha[1] / h, beta[0] + 1.5 * beta[1] / h]
-    d = [alpha[2], beta[2]]
-
-    a = np.insert(a, 1, 1 / h * (1 / h - 0.5 * p(x[1:-1])))
-    b = np.insert(b, 1, q(x[1:-1]) - 2 / h ** 2)
-    c = np.insert(c, 1, 1 / h * (1 / h + 0.5 * p(x[1:-1])))
-    d = np.insert(d, 1, f(x[1:-1]))
-
-    b[0] -= b[1] / a[1] * a[0]
-    c[0] -= c[1] / a[1] * a[0]
-    d[0] -= d[1] / a[1] * a[0]
-    np.delete(a, 0)
-
-    a[-1] -= a[-2] / c[-2] * c[-1]
-    b[-1] -= b[-2] / c[-2] * c[-1]
-    d[-1] -= d[-2] / c[-2] * c[-1]
-    np.delete(c, -1)
-
-    y = tridiag_solve((a, b, c), d)
-
-    return y
-
-
-def wave_equation_solve_1d(a, f, x_bounds, t_bounds, x_conditions, t_conditions, n_x=50, n_t=50):
+def wave_equation_solve_1d(a, f, x, t, x_conditions, t_conditions):
     """
-    solves the wave equation of the following form: u_tt - a**2*u_xx == f(x, t)
+    solves a wave equation of the following form: u_tt - a**2*u_xx == f(x, t)
 
     """
-    x = np.linspace(x_bounds[0], x_bounds[1], n_x)
-    t = np.linspace(t_bounds[0], t_bounds[1], n_t)
+    n_x, n_t = len(x), len(t)
     h = x[1] - x[0]
     tau = t[1] - t[0]
-    sigma = 0.25 * (1 - h ** 2 / tau ** 2)
+    sigma = 1. / 3 * (1 - (h / (a * tau)) ** 2)
     x, t = np.meshgrid(x, t)
     u = np.zeros((n_x, n_t))
     mu = x_conditions
@@ -72,17 +41,19 @@ def wave_equation_solve_1d(a, f, x_bounds, t_bounds, x_conditions, t_conditions,
     u[-1] = mu[1](t.transpose()[-1])
     u = u.transpose()
     u[0] = phi[0](x[0])
-    u[1] = u[0] + tau * phi[1](x[0]) + 0.5 * tau ** 2 * (a ** 2 * der(phi[0], x[0], dx=h, n=2) + f(x[0], 0))
-    kappa = a ** 2 * tau ** 2 / h ** 2
+    u[1] = u[0] + tau * phi[1](x[0]) + 0.5 * tau ** 2 * (a ** 2 * der(phi[0], x[0], dx=h, n=2) + f(x[0], t[0]))
+    kappa = (a * tau / h) ** 2
 
     for j in xrange(1, len(t) - 1):
-        d_sub = -0.5 * sigma * kappa * np.ones((n_x,))
+        d_sub = - sigma * kappa * np.ones((n_x - 1,))
         d_super = np.copy(d_sub)
-        d_main = 1 + 2 * sigma * kappa * np.ones((n_x,))
+        d_main = 1. + 2 * sigma * kappa * np.ones((n_x - 1,))
         diags = [d_sub, d_main, d_super]
-        rh = np.array(kappa * (1 - sigma) *
-                      (u[j - 1][2:] - 2 * u[j - 1][1:-1] + u[j - 1][:-2]) + tau ** 2 * f(x[j][1:-1], t[j][1:-1]) + 2 *
-                      u[j][1:-1] - u[j - 1][1:-1])
+        nonuniformity = np.array(f(x[j][1:-1], t[j][1:-1]))
+        rh = np.array(kappa *
+                      (1 - 2 * sigma) * (u[j - 1][2:] - 2 * u[j - 1][1:-1] + u[j - 1][:-2]) +
+                      sigma * (u[j][2:] - 2 * u[j][1:-1] + u[j][:-2]) + (tau ** 2) * nonuniformity
+                      + 2 * u[j][1:-1] - u[j - 1][1:-1])
         u[j + 1][1:-1] = tridiag_solve(diags, rh)
 
     return u
