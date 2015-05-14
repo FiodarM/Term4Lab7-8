7548f621f71ccc87387e2de90c3e4b4c50d01a08
FiodarM <fyodormorozko95@gmail.com>
2015-05-15 00:18:59 +0300

    Commit #2

--- wave_equation.py
+++ wave_equation.py
@@ -24,36 +24,6 @@ def tridiag_solve(diags, f):
     return x
 
 
-def ode_linear_2nd_order(coefs, bounds, conditions, n=50, f=lambda x: 0 * x):
-    p, q = coefs
-    x = np.linspace(bounds[0], bounds[1], n)
-    h = x[1] - x[0]
-    alpha, beta = conditions
-    a = [alpha[0] - 1.5 * alpha[1] / h, 0.5 * beta[1] / h]
-    b = [2 * alpha[1] / h, -2 * beta[1] / h]
-    c = [- 0.5 * alpha[1] / h, beta[0] + 1.5 * beta[1] / h]
-    d = [alpha[2], beta[2]]
-
-    a = np.insert(a, 1, 1 / h * (1 / h - 0.5 * p(x[1:-1])))
-    b = np.insert(b, 1, q(x[1:-1]) - 2 / h ** 2)
-    c = np.insert(c, 1, 1 / h * (1 / h + 0.5 * p(x[1:-1])))
-    d = np.insert(d, 1, f(x[1:-1]))
-
-    b[0] -= b[1] / a[1] * a[0]
-    c[0] -= c[1] / a[1] * a[0]
-    d[0] -= d[1] / a[1] * a[0]
-    np.delete(a, 0)
-
-    a[-1] -= a[-2] / c[-2] * c[-1]
-    b[-1] -= b[-2] / c[-2] * c[-1]
-    d[-1] -= d[-2] / c[-2] * c[-1]
-    np.delete(c, -1)
-
-    y = tridiag_solve((a, b, c), d)
-
-    return y
-
-
 def wave_equation_solve_1d(a, f, x, t, x_conditions, t_conditions):
     """
     solves a wave equation of the following form: u_tt - a**2*u_xx == f(x, t)
